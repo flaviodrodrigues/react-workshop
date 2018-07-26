3c9d5a572c57fb81a942c628ec2c6bb4da980ee7
Daniel Moreira <daniel.moreira@farfetch.com>
2018-07-27 00:05:03 +0100

    Eject app and add sytled-components plugin

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,22 +3,22 @@ import styled from 'styled-components';
 
 const AppContainer = styled.div`
   text-align: center;
-`;
+`
 
 const AppHeader = styled.header`
   background-color: #222;
   height: 150px;
   padding: 20px;
   color: white;
-`;
+`
 
 const AppTitle = styled.h1`
   font-size: 1.5em;
-`;
+`
 
 const AppIntro = styled.p`
   font-size: large;
-`;
+`
   
 class App extends Component {
   render() {
